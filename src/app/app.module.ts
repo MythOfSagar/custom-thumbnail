d8d27765ccae1c5d98b1679d99aea6cf6535305b
sagar1079 <sagarwankhade896@gmail.com>
2023-05-25 23:21:56 +0530

    Bug removal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YoutubeThumbnailComponent } from './components/youtube-thumbnail/youtube-thumbnail.component';
 import { FormsModule } from '@angular/forms';
+import { ToggleThemeComponent } from './components/toggle-theme/toggle-theme.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    YoutubeThumbnailComponent
+    YoutubeThumbnailComponent,
+    ToggleThemeComponent
   
   ],
   imports: [
