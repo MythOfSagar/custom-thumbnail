cb0af0a0a2e22781ae0da0e121d88e01bedb0ef0
sagar1079 <sagarwankhade896@gmail.com>
2023-06-30 16:33:52 +0530

    service worker checks

--- src/serviceworker/pwabuilder-adv-sw.js
+++ src/serviceworker/pwabuilder-adv-sw.js
@@ -1,3 +1,6 @@
 import {precacheAndRoute} from 'workbox-precaching/precacheAndRoute';
 
-precacheAndRoute([]);
+precacheAndRoute([{
+    "revision": "09a1f7c0f194d7abeb70e5f19d0786df",
+    "url": "app/components/button/button.component.html"
+  }]);
